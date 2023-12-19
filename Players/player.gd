e915954b83f56a41673083260e4c525986188cdc
danhoangg <hoang.daniel2005@gmail.com>
2023-12-19 14:15:23 +0000

    Animations between idle and walk

--- Players/player.gd
+++ Players/player.gd
@@ -1,12 +1,19 @@
 extends CharacterBody2D
 
 
-const SPEED = 100.0
+const SPEED = 175.0
 const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var animation_tree = $AnimationTree
+@onready var state_machine = animation_tree.get("parameters/playback")
+
+@export var starting_direction = 1
+
+func _ready():
+	update_animation_parameters(starting_direction)
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -16,13 +23,30 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	
+	# Change between idle and walk animation
+	pick_new_state()
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("left", "right")
 	if direction:
+		update_animation_parameters(direction)
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func update_animation_parameters(move_input : float):
+	if (move_input != 0):
+		animation_tree.set("parameters/Idle/blend_position", move_input)
+		animation_tree.set("parameters/Walk/blend_position", move_input)
+
+func pick_new_state():
+	if (velocity.x == 0):
+		state_machine.travel("Idle")
+	else:
+		state_machine.travel("Walk")
+
+
